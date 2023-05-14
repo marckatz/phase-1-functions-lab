bdc78dd8a22bfb35b3ab4f5df9bc0dc277bc5b95
Marc Katz <marc.f.katz@gmail.com>
2023-05-14 10:40:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(dest){
+    return Math.abs(dest - 42);
+}
+
+function distanceFromHqInFeet(dest){
+    return distanceFromHqInBlocks(dest) * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end)*264;
+}
+
+function calculatesFarePrice(start, end){
+    const dist = distanceTravelledInFeet(start, end);
+    if(dist < 400){
+        return 0;
+    }
+    else if(dist < 2000){
+        return 0.02 * (dist-400);
+    }
+    else if(dist < 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+
+}
